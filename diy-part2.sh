d21c83246e55ec4e5fd9ac36552427fe9a1403ca
a0575 <68583179+a0575@users.noreply.github.com>
2021-12-29 17:48:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 描述：DIY 脚本第2部分（Update feeds后）
+# 描述：DIY 脚本第2部分（Update feeds之后）
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
