315b85924411794b3d080f0456d3e3dd7b8388fa
a0575 <68583179+a0575@users.noreply.github.com>
2022-01-13 07:29:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -59,7 +59,6 @@ svn export --force https://github.com/coolsnowwolf/lede/trunk/tools/ucl tools/uc
 svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/generic/hack-5.10 target/linux/generic/hack-5.10
 rm -rf target/linux/generic/hack-5.10/{220-gc_sections*,781-dsa-register*}
 curl -sfL https://git.io/J0klM --create-dirs -o package/network/config/firewall/patches/fullconenat.patch
-) &
 
 sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
 sed -i 's/\/cgi-bin\/\(luci\|cgi-\)/\/\1/g' `find package/feeds/kiddin9/luci-*/ -name "*.lua" -or -name "*.htm*" -or -name "*.js"` &
