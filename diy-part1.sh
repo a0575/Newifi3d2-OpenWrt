127f6c6d11df88cca98c490dc6a321bad319515a
a0575 <68583179+a0575@users.noreply.github.com>
2022-06-01 13:04:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 说明：DIY 脚本第1部分（Update feeds之前）
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
